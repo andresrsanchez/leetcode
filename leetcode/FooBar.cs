c518209a9b036dbaeb5ff4de7f29ea16b81edb74
Andrés Rodríguez Sánchez <arodriguez@plainconcepts.com>
2020-07-15 08:52:12 +0200

    done with the autoresetevent mode on alternately foobar

---
+++ leetcode/FooBar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace leetcode
+{
+    public class FooBarAutoResetEvent
+    {
+        private int _n;
+        private AutoResetEvent _autoResetEvent = new AutoResetEvent(true);
+        private AutoResetEvent _autoResetEvent1 = new AutoResetEvent(false);
+
+        public FooBarAutoResetEvent(int n)
+        {
+            this._n = n;
+        }
+
+        public void Foo(Action printFoo)
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                _autoResetEvent.WaitOne();
+                printFoo();
+                _autoResetEvent1.Set();
+            }
+        }
+
+        public void Bar(Action printBar)
+        {
+            for (int i = 0; i < _n; i++)
+            {
+                _autoResetEvent1.WaitOne();
+                printBar();
+                _autoResetEvent.Set();
+            }
+        }
+    }
+}
